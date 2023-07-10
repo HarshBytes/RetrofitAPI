c458e916531f5ef3fb7a2cdd3fd28ab37b647df3
Harsh <harsh.zala.3114@gmail.com>
2023-07-10 16:16:07 +0530

    Navigation menu is created in which Logout button is moved

--- app/src/main/java/com/example/ecommdb/home.java
+++ app/src/main/java/com/example/ecommdb/home.java
@@ -1,39 +1,68 @@
 package com.example.ecommdb;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class home extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class home extends AppCompatActivity {
+public DrawerLayout drawerLayout;
+ActionBarDrawerToggle actionBarDrawerToggle;
+NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button Logout=findViewById(R.id.logout_btn);
-        Logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sp = getSharedPreferences("credential", MODE_PRIVATE);
+            setTitle("Food Cart");
+            navigationView=findViewById(R.id.navigation_menu);
+            drawerLayout=findViewById(R.id.drawer);
+            actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close);
+            drawerLayout.addDrawerListener(actionBarDrawerToggle);
+            actionBarDrawerToggle.syncState();
 
-                // Clear the username and password values
-                SharedPreferences.Editor editor = sp.edit();
-                editor.remove("username");
-                editor.remove("password");
-                editor.apply();
-                // Revert to the Login_Page
-                Intent intent=new Intent(home.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()){
+                        case R.id.nav_Logout:
+                            SharedPreferences sp = getSharedPreferences("credential", MODE_PRIVATE);
 
-                // Show a toast or perform any other action to indicate that the values have been deleted
-                Toast.makeText(getApplicationContext(), "Username and password deleted", Toast.LENGTH_SHORT).show();
-            }
-        });
+                            // Clear the username and password values
+                            SharedPreferences.Editor editor = sp.edit();
+                            editor.remove("username");
+                            editor.remove("password");
+                            editor.apply();
+                            // Revert to the Login_Page
+                            Intent intent=new Intent(home.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                            Toast.makeText(home.this, "User Logged Out", Toast.LENGTH_SHORT).show();
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                    }
+                    return false;
+                }
+            });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
